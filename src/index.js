bb961c4e245ce797a22da8df2488a018f4f5c787
tensiontocoding <91064624+tensiontocoding@users.noreply.github.com>
2022-05-05 15:33:22 +0900

    yes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createStore } from 'redux'; // 스토어생성
-import reducers from './reducers'; // 리듀서함수
+// import reducers from './reducers'; // 리듀서함수
 import { Provider } from 'react-redux'; // Store -> connect() -> Component 불러다 사용
 
-const store = createStore(reducers);
+// const store = createStore(reducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
